9eb14bed589dcf84ec0c173c71547630914413fd
saltsmall <mimi9693@gmail.com>
2021-03-05 18:11:11 +0900

    first commit

--- Programmers/pGymSuit.cpp
+++ Programmers/pGymSuit.cpp
@@ -11,26 +11,37 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 
     //도난당한 학생
     for(int i=0; i<lost.size(); i++){
-        student[lost[i]-1]--;
+        student[lost[i]-1] -= 1;
     } 
 
     //여벌복 가진 학생
     for(int i=0; i<reserve.size(); i++){
-        student[reserve[i]-1]++;
-        
-        if(reserve[i] != 1){
-            student[reserve[i]-2]++;
+        student[reserve[i]-1] += 1;
+    }
+    
+    //여벌복 빌려주기
+    for(int i=0; i<student.size(); i++){
+        if(student[i] == 1){
+            if(i-1>-1){
+                if(student[i-1] == -1) {
+                    student[i-1] += 1;
+                    student[i] -= 1;
+                }
+            }
         }
-        if(student[i] != n){
-            student[reserve[i]]++;
+        if(student[i] == 1){
+            if(i+1<student.size()){
+                if(student[i+1] == -1){
+                    student[i+1] += 1;
+                    student[i] -= 1;
+                }
+            }
         }
     }
-
+    
     //체육복 없는 학생 수 세기
-
-    for(int i=0; i<n; i++){
-        if(student[i] != -1) answer++; 
+    for(int i=0; i<student.size(); i++){
+        if(student[i] == 0 || student[i] == 1) ++answer; 
     }
-
     return answer;
 }
