5c6431c51e61fb138651bad4d4ae1c00bd31022a
saltsmall <mimi9693@gmail.com>
2021-03-12 21:26:03 +0900

    이진탐색법

---
+++ Search/p2110.cpp
@@ -0,0 +1,49 @@
+/*
+https://www.acmicpc.net/problem/2110
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int ans;
+    int N,C;
+    cin >> N >> C;
+    
+    vector<int> houses;
+    for(int i=0; i<N; i++){
+        int num;
+        cin >> num;
+        houses.push_back(num);
+    }
+    sort(houses.begin(), houses.end());
+
+    int min = 1;
+    int max = houses.back()-houses.front();
+    cout<< max << "\n";
+    while(min<=max){
+        int mid = (min+max)/2;
+        int c = 1;
+        for(int i=1;i<houses.size();i++){
+            if(mid <= (houses[i]-houses[i-1])){
+                c++;
+            }
+        }
+        cout << "max : " << max << " min : " << min << "\n";
+        cout << "mid : " << mid << " c : " << c << "\n";
+        if(c<C){
+            max = mid -1;
+        }
+        else{
+            if(mid<=max){
+                ans = mid;
+            }
+            min = mid +1;
+        }
+        cout << "\n\n";
+    }
+    cout << ans;
+}
