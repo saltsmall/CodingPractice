1266ccaed1bd6bd3d82aa58a625e0bd6eb622cbb
saltsmall <mimi9693@gmail.com>
2021-03-12 22:02:28 +0900

    이진탐색법

--- Search/p1654.cpp
+++ Search/p1654.cpp
@@ -3,6 +3,7 @@ https://www.acmicpc.net/problem/1654
 */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,7 +12,7 @@ int main(){
     int K, N;
     cin >> K >> N;
 
-    vector<int> lenWires;
+    vector<long long> lenWires;
     int sum = 0;
     for(int i=0; i<K; i++){
         int num;
@@ -19,32 +20,28 @@ int main(){
         sum += num;
         lenWires.push_back(num);
     }
-    //cout << "sum :" << sum << "\n";
-    int min = 1;
-    int max = sum/N;
+
+    sort(lenWires.begin(), lenWires.end());
+
+    long long min = 1;
+    long long max = lenWires.back();
     
     while (min <= max) {
-        int mid = (min + max)/2;
+        long long mid = (min + max)/2;
         int n =0;
-        //cout << "min : " << min << "\n";
-        //cout << "mid : " << mid << "\n";
-        //cout << "max : " << max << "\n";
 
-        for(int wire: lenWires){
+        for(long long wire: lenWires){
             n += wire/mid;
         }
-        //cout << "n : " << n << "\n";
         if(n < N){
             max = mid -1;
         }
         else{
             if(mid<=max){
                 ans = mid;
-                //cout << "ans : " << mid << "\n";
             }
             min = mid +1;
         }
-        //cout << "\n\n";
     }
 
     cout << ans;
