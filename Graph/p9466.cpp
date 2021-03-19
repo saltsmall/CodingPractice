166fac609c4e4857952ef5bdabe2cab8be8520f0
saltsmall <mimi9693@gmail.com>
2021-03-19 22:34:20 +0900

    DFS/BFS

--- Graph/p9466.cpp
+++ Graph/p9466.cpp
@@ -8,28 +8,46 @@ https://www.acmicpc.net/problem/9466
 
 using namespace std;
 
-vector<int> vec(100000);
-vector<bool> visited(100000, false);
-int ans;
+vector<int> vec(100001);
+vector<int> visited(100001, false);
+vector<bool> team(100001, false);
+int N, cnt;
 
 void DFS(int cur){
-    if(vec[cur] == cur){}
-
+    //더이상 진행하지 않아도 될 때
+    if(visited[cur] == -1 || team[cur]) return ; 
+    
+    if(visited[cur] == 0) visited[cur]++;
+    else if(visited[cur] == 1){
+        //team에 속함
+        team[cur] = true;
+        cnt ++;
+    }
+    //한바퀴를 더 돈다.
+    DFS(vec[cur]);
+    visited[cur] = -1;
 }
 
 int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
     int T;
     cin >> T;
 
     for(int t=0; t<T; t++){
-        ans = 0;
-        int N;
+        cnt = 0;
         cin >> N;
         for(int n=1; n<=N; n++){
             cin >> vec[n];
+            visited[n] = false;
+            team[n] = false;
         }
 
-        
+        for(int n=1; n<=N; n++){
+            if(!visited[n]) DFS(n);
+        }
+        int ans = N-cnt;
+        cout << ans << "\n";
     }
 
     return 0;
